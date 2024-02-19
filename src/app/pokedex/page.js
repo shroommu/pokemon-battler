cdbaed2fcec84034243a00d32bee6edba13595f3
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-18 22:33:22 -0800

    test removing fetch

--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -1,13 +1,10 @@
 import PokedexContext from "./components/pokedexContext";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ log: ["query", "info"] });
 
 export default async function Pokedex() {
-  console.log(process.env.URL + "/api/pokedex/getPokemonList/");
-  const { result: pokemons } = await fetch(
-    process.env.URL + "/api/pokedex/getPokemonList/",
-    { method: "GET" }
-  ).then((res) => {
-    return res.json();
-  });
+  const pokemons = await prisma.pokemon.findMany({});
 
   return (
     <div testid="container" className="flex grow flex-row h-auto w-auto">
