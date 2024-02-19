f30ff109e8bb638495db9d22ff247df979d37448
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-18 21:53:25 -0800

    add debug statement

--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -1,12 +1,13 @@
-"use server";
-
 import PokedexContext from "./components/pokedexContext";
 
 export default async function Pokedex() {
   const { result: pokemons } = await fetch(
     process.env.URL + "/api/pokedex/getPokemonList/",
     { method: "GET" }
-  ).then((res) => res.json());
+  ).then((res) => {
+    console.log(res);
+    return res.json();
+  });
 
   return (
     <div testid="container" className="flex grow flex-row h-auto w-auto">
