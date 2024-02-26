175b31fac68cd36220c00bca27c91ea31261754b
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-25 23:18:11 -0800

    add prefetch to pokemon list links

--- src/app/pokedex/components/pokemonList.js
+++ src/app/pokedex/components/pokemonList.js
@@ -28,6 +28,7 @@ export default function PokemonList({ pokemons }) {
                 ? pokemon.name.replace(" ", "-").toLowerCase()
                 : `pokedex/${pokemon.name.replace(" ", "-").toLowerCase()}`
             }
+            prefetch
             key={pokemon.name}
           >
             <li
