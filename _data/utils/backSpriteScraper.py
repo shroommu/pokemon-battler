9b3686cc85f14c0067b6cf9e85eb753c33d16341
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-25 18:46:15 -0800

    update sprites to gen 3 versions

--- _data/utils/backSpriteScraper.py
+++ _data/utils/backSpriteScraper.py
@@ -7,10 +7,10 @@ def save_all_back_sprites():
     pokemon_list = pokemon_df.values.tolist();
 
     for pokemon in pokemon_list:
-        req = request.Request(f"https://img.pokemondb.net/sprites/silver/back-normal/{pokemon[0].lower()}.png", None, {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'})
+        req = request.Request(f"https://img.pokemondb.net/sprites/ruby-sapphire/back-normal/{pokemon[0].replace(' ', '-').lower()}.png", None, {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'})
         data = request.urlopen(req)
         img_data = data.read()
-        file_path = f'./public/images/pokemon/sprites/back/{pokemon[0].lower()}.png'
+        file_path = f'./public/images/pokemon/sprites/back/{pokemon[0].replace(' ', '-').lower()}.png'
 
         with open(file_path, 'wb') as handler:
             handler.write(img_data)
