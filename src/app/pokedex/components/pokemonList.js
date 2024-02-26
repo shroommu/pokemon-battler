9b60a3b7400fa9d24ac4e74b36c238b59b2f8686
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-25 20:30:11 -0800

    add selected state to pokemon list item

--- src/app/pokedex/components/pokemonList.js
+++ src/app/pokedex/components/pokemonList.js
@@ -2,6 +2,17 @@
 
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { tv } from "tailwind-variants";
+
+const pokemonListButtonStyle = tv({
+  base: "group py-2 px-4 mb-1 bg-gray-400 rounded-md hover:bg-gray-300 active:bg-gray-500 w-full",
+  variants: {
+    state: {
+      selected:
+        "group py-2 px-4 mb-1 bg-gray-300 rounded-md hover:bg-gray-200 active:bg-gray-400 w-full",
+    },
+  },
+});
 
 export default function PokemonList({ pokemons }) {
   const router = useRouter();
@@ -13,7 +24,10 @@ export default function PokemonList({ pokemons }) {
         return (
           <li
             key={pokemon.name}
-            className="group py-2 px-4 mb-1 bg-gray-400 rounded-md hover:bg-gray-300 active:bg-gray-500 w-full"
+            className={pokemonListButtonStyle({
+              state:
+                pathname.includes(pokemon.name.toLowerCase()) && "selected",
+            })}
           >
             <button
               onClick={() =>
