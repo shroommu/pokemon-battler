f1b2da57a9b0bde6aaf9b2c905d62f91185bc4eb
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-03-10 12:59:16 -0700

    start setting up next-auth

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,47 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { getSession } from "next-auth/react";
 
 import Button from "./Button";
 import { locations } from "@/app/constants";
 
 export default function Nav({}) {
+  const { data: session } = getSession();
+
   return (
     <nav className="flex">
       <ul className="flex flex-row items-center">
-        {/* <li>
-          <Link href={locations.INDEX}>
-            <Button margin="mr-2" fontSize="text-2xl" extraClasses={'hover:bg-red-500 active:bg-red-700'}>
-              Home
-            </Button>
-          </Link>
-        </li> */}
         <li>
           <Link href={locations.POKEDEX}>
-            <Button margin="mr-2" fontSize="text-2xl" extraClasses={'hover:bg-red-500 active:bg-red-700'}>
+            <Button
+              margin="mr-2"
+              fontSize="text-2xl"
+              extraClasses={"hover:bg-red-500 active:bg-red-700"}
+            >
               Pokedex
             </Button>
           </Link>
         </li>
       </ul>
+      {session ? (
+        <Button
+          margin="ml-auto"
+          fontSize="text-2xl"
+          extraClasses={"hover:bg-red-500 active:bg-red-700"}
+        >
+          Account
+        </Button>
+      ) : (
+        <Button
+          margin="ml-auto"
+          fontSize="text-2xl"
+          extraClasses={"hover:bg-red-500 active:bg-red-700"}
+        >
+          Sign In
+        </Button>
+      )}
     </nav>
   );
 }
