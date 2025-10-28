4ffce529e9365a5220cae410be72a72cf4c13523
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-10-28 19:30:22 +0000

    add tooltip container variants

--- src/components/charts/components/Tooltip.js
+++ src/components/charts/components/Tooltip.js
@@ -1,15 +1,13 @@
 import { tv } from "tailwind-variants";
 
-const tooltipArrowClass = tv({
-  base: "absolute h-0 w-0",
+const tooltipContainerClass = tv({
+  base: "absolute",
   variants: {
     position: {
-      top: "left-1/2 -translate-x-1/2 top-full border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white",
-      bottom:
-        "left-1/2 -translate-x-1/2 bottom-full border-l-4 border-r-4 border-b-4 border-l-transparent border-r-transparent border-b-white",
-      left: "top-1/2 -translate-y-1/2 left-full border-t-4 border-b-4 border-l-4 border-t-transparent border-b-transparent border-l-white",
-      right:
-        "top-1/2 -translate-y-1/2 right-full border-t-4 border-b-4 border-r-4 border-t-transparent border-b-transparent border-r-white",
+      top: "-translate-x-1/2",
+      bottom: "-translate-x-1/2",
+      left: "-translate-y-1/2",
+      right: "-translate-y-1/2",
     },
   },
 });
@@ -22,6 +20,20 @@ const tooltipBodyClass = tv({
       bottom: "mt-2",
       left: "mr-2",
       right: "ml-2",
+    },
+  },
+});
+
+const tooltipArrowClass = tv({
+  base: "absolute h-0 w-0",
+  variants: {
+    position: {
+      top: "left-1/2 -translate-x-1/2 top-full border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-white",
+      bottom:
+        "left-1/2 -translate-x-1/2 bottom-full border-l-4 border-r-4 border-b-4 border-l-transparent border-r-transparent border-b-white",
+      left: "top-1/2 -translate-y-1/2 left-full border-t-4 border-b-4 border-l-4 border-t-transparent border-b-transparent border-l-white",
+      right:
+        "top-1/2 -translate-y-1/2 right-full border-t-4 border-b-4 border-r-4 border-t-transparent border-b-transparent border-r-white",
     },
   },
 });
@@ -39,7 +51,9 @@ export default function Tooltip({ interactionData, position }) {
         left: xPos,
         top: yPos,
       }}
-      className="absolute -translate-y-1/2"
+      className={tooltipContainerClass({
+        position: position,
+      })}
       data-testid="tooltip-container"
     >
       <div
