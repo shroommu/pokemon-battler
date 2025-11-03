502c13a52f982383f1a287dad1ac6f2ab05f79a0
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-03 17:08:34 +0000

    custom prisma query -> getAllPokemon

--- src/app/analytics/page.js
+++ src/app/analytics/page.js
@@ -1,31 +1,8 @@
-import prisma from "@/lib/prisma";
-
 import Dashboard from "./components/dashboard";
+import { getAllPokemon } from "@/services/getAllPokemon";
 
-export default async function AnalyticsPage({ children }) {
-  const pokemons = await prisma.pokemon.findMany({
-    include: {
-      primary_type: true,
-      secondary_type: true,
-      pokemon_moves: {
-        select: {
-          move: {
-            select: {
-              name: true,
-              power: true,
-              accuracy: true,
-              pp: true,
-              effect: true,
-              type: { select: { name: true } },
-            },
-          },
-        },
-      },
-    },
-    orderBy: [{ pokedex_number: "asc" }],
-  });
+export default async function AnalyticsPage() {
+  const pokemons = await getAllPokemon();
 
-  return (
-    <Dashboard pokemons={pokemons}/>
-  );
+  return <Dashboard pokemons={pokemons.data} />;
 }
