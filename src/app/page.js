4bf4eb1fa89b9c0f5576e17cf0df802025152db4
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-03 20:50:58 +0000

    change some test ids

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ export default function Home() {
     <div data-testid="container" className="flex flex-row h-auto w-full">
       <div
         className="flex flex-col m-4 h-auto md:w-full"
-        data-testid="pokedex-home-page-container"
+        data-testid="home-page-container"
       >
         <section
           className="flex flex-col p-6 w-full bg-gray-200 rounded-md items-center h-full"
-          data-testid="pokedex-home-page"
+          data-testid="home-page"
         >
           <h1 className="text-xl mb-4">
             Welcome to the Wonderful World of Pokemon!
@@ -17,7 +17,7 @@ export default function Home() {
             development and data visualization skills.
           </p>
           <p>
-            I&apos;m using NextJS, postgreSQL, TailwindCSS, and a few other
+            I&apos;m using NextJS, postgreSQL, TailwindCSS, D3.js and a few other
             tools and libraries to make the magic happen.
           </p>
           <p>Thanks for visiting!</p>
