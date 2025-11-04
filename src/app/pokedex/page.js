e76e448dab2e9cf6e5aadb8ef2d28bd66f2f33be
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-04 05:28:03 -0800

    fill hiehgt when pokedex is empty

--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -1,6 +1,6 @@
 export default async function PokedexLandingPage() {
   return (
-    <div className="flex h-[calc(100dvh-60px-104px-51.2px-24px)] md:h-[calc(100dvh-120px-104px-51.2px-24px)] w-full justify-center">
+    <div className="flex h-[calc(100dvh-60px-104px-51.2px-24px)] md:h-[calc(100dvh-120px-104px-51.2px)] w-full justify-center">
       Select a Pokemon from the list
     </div>
   );
