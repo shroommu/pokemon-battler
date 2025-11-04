ca1022c6f0f15b8f4c2f18c0863755e976ed2a79
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-04 23:35:56 +0000

    messing with architecture changes

--- src/app/pokedex/[pokemon]/page.js
+++ src/app/pokedex/[pokemon]/page.js
@@ -1,4 +1,9 @@
 import PokedexEntry from "./components/pokedexEntry";
+import { getAllPokemon } from "@/services/getAllPokemon";
+
+import PokemonList from "./components/pokemonList";
+import PokemonListDropdown from "./components/pokemonListDropdown";
+import Tabs from "./components/Tabs";
 import { getUniquePokemonByName } from "@/services/getUniquePokemonByName";
 
 export default async function PokedexEntryContainer({ params }) {
@@ -10,11 +15,41 @@ export default async function PokedexEntryContainer({ params }) {
     return capitalizedWords.join(" ");
   }
 
+  const pokemons = await getAllPokemon();
   const pokemon = await getUniquePokemonByName(
     capitalizePokemonSlug(params.pokemon)
   );
 
-  return <PokedexEntry pokemon={pokemon.data} />;
+  return (
+    <div
+      data-testid="pokedex-container"
+      className="relative flex flex-col md:flex-row h-full w-full"
+    >
+      <div
+        className="hidden flex-col lg:mr-0 m-4 flex-none lg:flex h-auto"
+        data-testid="pokemon-list-container"
+      >
+        <PokemonList pokemons={pokemons.data} />
+      </div>
+      <div
+        className="flex flex-col m-4 mb-0 items-center lg:hidden"
+        data-testid="pokemon-list-mobile-dropdown-container"
+      >
+        <PokemonListDropdown pokemons={pokemons.data} />
+      </div>
+      <div className="absolute right-8 -top-8">
+        <Tabs />
+      </div>
+      <div className="flex flex-col w-full p-4" data-testid="tabs-container">
+        <div
+          className="flex w-full p-4 pt-12 bg-gray-200 rounded-md"
+          data-testid="pokemon-data-container"
+        >
+          <PokedexEntry pokemon={pokemon.data} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export const dynamic = "force-dynamic";
--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -1,7 +1,4 @@
-export default async function PokedexLandingPage() {
-  return (
-    <div className="flex h-[calc(100dvh-60px-104px-51.2px-24px)] md:h-[calc(100dvh-120px-104px-51.2px)] w-full justify-center">
-      Select a Pokemon from the list
-    </div>
-  );
+export default async function PokedexLandingPage({ children }) {
+
+  return children;
 }
