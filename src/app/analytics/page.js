9b336a6edad5b7cf8065b5f4c3f0d3bfd2f36526
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-06 16:57:54 +0000

    delete analytics route

--- src/app/analytics/page.js
+++
@@ -1,8 +0,0 @@
-import Dashboard from "../pokedex/[pokemon]/components/Stats";
-import { getAllPokemon } from "@/services/getAllPokemon";
-
-export default async function AnalyticsPage() {
-  const pokemons = await getAllPokemon();
-
-  return <Dashboard pokemons={pokemons.data} />;
-}
