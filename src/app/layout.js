c36f4453bfa520d47f17f464115627b26f429a2b
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-06 23:16:01 +0000

    fix responsive height issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <body className="h-dvh bg-red-500">
         <Header />
         <div
-          className="flex h-auto bg-red-500"
+          className="flex flex-none min-h-[calc(100dvh-120px)] bg-red-500"
           data-testid="content-body-container"
         >
           {children}
--- src/app/pokedex/[pokemon]/layout.js
+++ src/app/pokedex/[pokemon]/layout.js
@@ -10,7 +10,7 @@ export default async function PokedexEntryContainer({ params, children }) {
   return (
     <div
       data-testid="pokedex-container"
-      className="relative flex flex-col md:flex-row h-full w-full"
+      className="relative flex flex-col items-stretch md:flex-row h-auto w-full"
     >
       <div
         className="hidden flex-col lg:mr-0 m-4 flex-none lg:flex h-auto"
@@ -29,7 +29,7 @@ export default async function PokedexEntryContainer({ params, children }) {
       </div>
       <div className="flex flex-col w-full p-4" data-testid="tabs-container">
         <div
-          className="flex w-full p-4 pt-12 bg-gray-200 rounded-md"
+          className="flex h-full w-full p-4 pt-12 bg-gray-200 rounded-md"
           data-testid="pokemon-data-container"
         >
           {children}
