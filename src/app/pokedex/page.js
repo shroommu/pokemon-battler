6aa8ed54b223b91bdccfca19426bdbcb573ddcc6
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-08 07:49:03 -0800

    flesh out more of the static site content

--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -14,9 +14,14 @@ export default async function PokedexHome() {
           className="flex flex-col p-6 w-full bg-gray-200 rounded-md items-center h-full"
           data-testid="home-page"
         >
-          <h1 className="text-xl mb-4">Pokedex</h1>
+          <h1 className="text-2xl mb-4">Pokedex</h1>
+          <p className="mb-4 text-center">
+            Welcome to the Pokedex! Select a Pokemon from below to view its
+            stats and information from the original Red, Blue, and Yellow
+            Pokemon games.
+          </p>
           <div>
-            <ul className="grid gap-1 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+            <ul className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
               {pokemons?.data.map((pokemon) => (
                 <PokedexButton
                   key={pokemon.name}
