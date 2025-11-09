eb2e9127e60ce7358bea56a23288ad781da7c9bb
Alexa Kruckenberg <mouldy.k@gmail.com>
2025-11-08 16:33:30 -0800

    more speed optimizing

--- src/app/pokedex/[pokemon]/stats/page.js
+++ src/app/pokedex/[pokemon]/stats/page.js
@@ -25,21 +25,10 @@ export default async function Stats({ params }) {
   );
 
   return (
-    <div className="flex flex-col w-full" data-testid="pokemon-entry-container">
-      {pokemon ? (
-        <PokemonDataEntry
-          pokemon={pokemon.data}
-          previousPokemon={previousPokemon?.data}
-          nextPokemon={nextPokemon?.data}
-        />
-      ) : (
-        <section
-          className="flex flex-col p-4 h-full w-full bg-gray-200 rounded-md items-center"
-          data-testid="loading-container"
-        >
-          loading...
-        </section>
-      )}
-    </div>
+    <PokemonDataEntry
+      pokemon={pokemon.data}
+      previousPokemon={previousPokemon?.data}
+      nextPokemon={nextPokemon?.data}
+    />
   );
 }
