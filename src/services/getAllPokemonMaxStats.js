ee3511b1eaa92743f7ba3153e15e40ffc6364ff3
Alex Kruckenberg <mouldy.k@gmail.com>
2026-02-05 19:07:55 +0000

    new service returning pokemonData with sum of stats

---
+++ src/services/getAllPokemonMaxStats.js
@@ -0,0 +1,26 @@
+"use server";
+
+import { cache } from "react";
+
+import prisma from "@/lib/prisma";
+
+import { sum } from "mathjs";
+
+export const getAllPokemonMaxStats = cache(async () => {
+  const pokemons = await prisma.pokemon.findMany();
+
+  const data = pokemons.map((pokemon) => {
+    return {
+      ...pokemon,
+      maxStats: sum([
+        pokemon.hp,
+        pokemon.attack,
+        pokemon.defense,
+        pokemon.special,
+        pokemon.speed,
+      ]),
+    };
+  });
+
+  return { data };
+});
