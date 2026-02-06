158607e946395658d6d75020e3d6e31ba66ab4b4
Alex Kruckenberg <mouldy.k@gmail.com>
2026-02-06 01:05:28 +0000

    normalize in component instead of in db service call

--- src/components/charts/BoxPlot/BoxPlotItem.js
+++ src/components/charts/BoxPlot/BoxPlotItem.js
@@ -1,18 +1,20 @@
+import { range } from "d3";
+
 export default function BoxPlotItem({ data, width, height, yPos }) {
   return (
     <g data-testid="box-plot-item-group">
       <g data-testid="box-plot-left-whisker-group">
         <line
-          x1={width * data.leftWhisker}
-          x2={width * data.leftWhisker}
+          x1={(width * data.min) / data.max}
+          x2={(width * data.min) / data.max}
           y1={yPos - height / 2}
           y2={yPos + height / 2}
           stroke="black"
           strokeWidth={2}
         />
         <line
-          x1={width * data.leftWhisker}
-          x2={width * data.q1}
+          x1={(width * data.min) / data.max}
+          x2={(width * data.q1) / data.max}
           y1={yPos}
           y2={yPos}
           stroke="black"
@@ -20,9 +22,9 @@ export default function BoxPlotItem({ data, width, height, yPos }) {
         />
       </g>
       <rect
-        width={width * data.q3 - width * data.q1}
+        width={(width * data.q3) / data.max - (width * data.q1) / data.max}
         height={height}
-        x={width * data.q1}
+        x={(width * data.q1) / data.max}
         y={yPos - height / 2}
         fill={data.fillColor}
         stroke="black"
@@ -30,8 +32,8 @@ export default function BoxPlotItem({ data, width, height, yPos }) {
         data-testid="quantile-box"
       />
       <line
-        x1={width * data.mean}
-        x2={width * data.mean}
+        x1={(width * data.mean) / data.max}
+        x2={(width * data.mean) / data.max}
         y1={yPos - height / 2}
         y2={yPos + height / 2}
         stroke="black"
@@ -40,16 +42,16 @@ export default function BoxPlotItem({ data, width, height, yPos }) {
       />
       <g data-testid="box-plot-left-whisker-group">
         <line
-          x1={width * data.rightWhisker}
-          x2={width * data.rightWhisker}
+          x1={(width * data.max) / data.max}
+          x2={(width * data.max) / data.max}
           y1={yPos - height / 2}
           y2={yPos + height / 2}
           stroke="black"
           strokeWidth={2}
         />
         <line
-          x1={width * data.q3}
-          x2={width * data.rightWhisker}
+          x1={(width * data.q3) / data.max}
+          x2={(width * data.max) / data.max}
           y1={yPos}
           y2={yPos}
           stroke="black"
@@ -60,7 +62,7 @@ export default function BoxPlotItem({ data, width, height, yPos }) {
         {data.dataPoints.map((dataPoint) => (
           <circle
             r={4}
-            cx={dataPoint.normalizedValue * width}
+            cx={(dataPoint.value * width) / data.max}
             cy={yPos}
             key={dataPoint.name}
             data-testid={dataPoint.name}
