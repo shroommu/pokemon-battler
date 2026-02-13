f85ef206077c398cc5664b8fda50f955aed8cca4
Alex Kruckenberg <mouldy.k@gmail.com>
2026-02-13 00:15:05 +0000

    basic static histogram

--- src/components/charts/Histogram/Histogram.js
+++ src/components/charts/Histogram/Histogram.js
@@ -1,3 +1,39 @@
-export default function Histogram({}) {
-  return <div data-testid="histogram-container"></div>;
+import { scaleBand, scaleLinear } from "d3";
+
+const DATA = {
+  bins: [
+    { value: 1, count: 5 },
+    { value: 2, count: 7 },
+    { value: 3, count: 2 },
+  ],
+};
+
+export default function Histogram({ width = 600, height = 400, data = DATA }) {
+  const xScale = scaleBand()
+    .domain(data.bins.map((d) => d.value))
+    .range([0, width])
+    .padding(0.05);
+
+  const yScale = scaleLinear().domain([0, 10]).range([0, height]);
+
+  const bars = data.bins.map((bin) => {
+    return (
+      <rect
+        x={xScale(bin.value)}
+        y={height - yScale(bin.count)}
+        width={xScale.bandwidth()}
+        height={yScale(bin.count)}
+        fill="blue"
+      />
+    );
+  });
+
+  return (
+    <div data-testid="histogram-container">
+      <svg height={height} width={width}>
+        <rect height={height} width={width} fill="white" />
+        {bars}
+      </svg>
+    </div>
+  );
 }
