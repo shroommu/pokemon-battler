0ed4a3dbc52c6b58dce94a6021bfb9d4b83471e4
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-02-15 19:25:03 -0800

    exclude images in matching

--- src/middleware.js
+++ src/middleware.js
@@ -52,5 +52,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|.*\\..*).*)"],
 };
