180a2dc688a6ee61ec429cd33f58c72798e0ed50
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-02-21 05:12:49 -0800

    Move Storybook stories into __stories__ folders

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,6 +1,6 @@
 /** @type {import("@storybook/nextjs-vite").StorybookConfig} */
 const config = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/__stories__/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-docs",
